851ab78f29a6b12b5297c56e1317b5d814539b3e
TheSaibankan <51401079+TheSaibankan@users.noreply.github.com>
2019-06-28 11:53:28 +0200

    gameBoucle

--- src/main.java
+++ src/main.java
@@ -8,6 +8,7 @@ public class main {
         Random random               = new Random();
         Scanner sc                  = new Scanner(System.in);
 
+        boolean gameBoucle          = true;
         boolean menuBoucle          = true;
         boolean challengerBoucle    = false;
         boolean defenseurBoucle     = false;
@@ -17,68 +18,72 @@ public class main {
 
         System.out.println("Bienvenue dans le module de recherche d'une combinaison à X chiffres.");
 
-        while (menuBoucle   = true) {
-            System.out.println("Veuillez sélectionner un mode de jeu :\n1 - Challenger  2 - Défenseur  3 - Duel");
+        while (gameBoucle){
 
-            int reponseMode = sc.nextInt();
+            while (menuBoucle) {
+                System.out.println("Veuillez sélectionner un mode de jeu :\n1 - Challenger  2 - Défenseur  3 - Duel");
 
-            if (reponseMode == 1) {
-                menuBoucle          = false;
-                challengerBoucle    =  true;
-            }
+                int reponseMode = sc.nextInt();
 
-            else if (reponseMode == 2) {
-                menuBoucle          = false;
-                defenseurBoucle     = true;
-            }
-
-            else if (reponseMode == 3) {
-                menuBoucle          = false;
-                duelBoucle          = true;
-            }
-
-            else {menuBoucle = true;}
-
-        while (challengerBoucle == true) {
-
-            System.out.println("Vous avez sélectionné le mode Challenger !");
-            System.out.println("Par défaut, la combinaison contient 4 chiffres. Voulez-vous modifier la difficulté ?\n 1 - Oui  2 - Non");
-
-            int reponseDiff = sc.nextInt();
-
-            if (reponseDiff == 1) {
-                System.out.println("Choisissez la difficulté : 1 - Facile  2 - Normal  3 - Difficile  4 - Personnalisé");
-
-                int choixDiff = sc.nextInt();
-
-                if (choixDiff == 1){
-                    System.out.println("Vous avez choisi le mode facile.");
-                    longueurCombinaison = 3;
+                if (reponseMode == 1) {
+                    menuBoucle          = false;
+                    challengerBoucle    =  true;
                 }
 
-                else if (choixDiff == 2) {
-                    System.out.println("Vous avez choisi le mode normal.");
-                    longueurCombinaison = 4;
+                else if (reponseMode == 2) {
+                    menuBoucle          = false;
+                    defenseurBoucle     = true;
                 }
 
-                else if (choixDiff == 3) {
-                    System.out.println("Vous avez choisi le mode difficile.");
-                    longueurCombinaison = 5;
+                else if (reponseMode == 3) {
+                    menuBoucle          = false;
+                    duelBoucle          = true;
                 }
 
-                else if (choixDiff == 4) {
-                    System.out.println("Choisissez la longueur de la combinaison.");
-                    int userLongueurCombi = sc.nextInt();
+                else {menuBoucle = true;}
 
+                while (challengerBoucle) {
+
+                    System.out.println("Vous avez sélectionné le mode Challenger !");
+                    System.out.println("Par défaut, la combinaison contient 4 chiffres. Voulez-vous modifier la difficulté ?\n 1 - Oui  2 - Non");
+
+                    int reponseDiff = sc.nextInt();
+
+                    if (reponseDiff == 1) {
+                        System.out.println("Choisissez la difficulté : 1 - Facile  2 - Normal  3 - Difficile  4 - Personnalisé");
+
+                        int choixDiff = sc.nextInt();
+
+                        if (choixDiff == 1){
+                            System.out.println("Vous avez choisi le mode facile.");
+                            longueurCombinaison = 3;
+                        }
+
+                        else if (choixDiff == 2) {
+                            System.out.println("Vous avez choisi le mode normal.");
+                            longueurCombinaison = 4;
+                        }
+
+                        else if (choixDiff == 3) {
+                            System.out.println("Vous avez choisi le mode difficile.");
+                            longueurCombinaison = 5;
+                        }
+
+                        else if (choixDiff == 4) {
+                            System.out.println("Choisissez la longueur de la combinaison.");
+                            int userLongueurCombi = sc.nextInt();
+                            longueurCombinaison = userLongueurCombi;
+
+                        }
+
+                        else {System.out.println("Une erreur est survenue. Redémarrage du module Challenger...");}
+                    }
+
+                    else if (reponseDiff == 2) {continue;}
+
+                    else{System.out.println("Une erreur est survenue. Redémarrage du module Challenger...");}
                 }
-
-                else {System.out.println("Une erreur est survenue. Redémarrage du module Challenger...");}
             }
-
-            else if (reponseDiff == 2) {continue;}
-
-            else{System.out.println("Une erreur est survenue. Redémarrage du module Challenger...");}
-        }
 
 
         }
