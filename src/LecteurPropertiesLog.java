118697d80b362ef78322ce4210f4bf3ceb588417
TheSaibankan <51401079+TheSaibankan@users.noreply.github.com>
2019-07-18 16:18:24 +0200

    ajouts Class Log

--- src/LecteurPropertiesLog.java
+++ src/LecteurPropertiesLog.java
@@ -1,2 +1,14 @@
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Logger;
+
 public class LecteurPropertiesLog {
+    Logger logger = Logger.getLogger(Main.class);
+
+    void TestLog() {
+        ConsoleAppender appender = (ConsoleAppender)  logger.getAppender("console");
+        logger.addAppender(appender);
+        logger.debug("Hello world.");
+        logger.info("What a beautiful day.");
+        logger.warn("What a beautiful day.");
+    }
 }
