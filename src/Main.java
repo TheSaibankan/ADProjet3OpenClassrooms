e0f3b264858a5fc5944333f0c0c76d1f8216de9d
TheSaibankan <51401079+TheSaibankan@users.noreply.github.com>
2019-07-25 10:32:25 +0200

    modification des noms en anglais

--- src/Main.java
+++ src/Main.java
@@ -2,42 +2,42 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    static boolean challengerBoucle = false;
-    static boolean defenseurBoucle = false;
-    static boolean duelBoucle = false;
-    static boolean menuBoucle;
+    static boolean challengerLoop   = false;
+    static boolean defenderLoop     = false;
+    static boolean duelLoop         = false;
+    static boolean menuLoop         = true;
 
     public static void main(String[] args) throws IOException {
 
         Scanner sc                  = new Scanner(System.in);
-        boolean menuBoucle          = true;
+
 
         System.out.println("Bienvenue dans le module de recherche d'une combinaison à X chiffres.");
 
-        while (menuBoucle) {
+        while (menuLoop) {
 
             System.out.println("Veuillez sélectionner un mode de jeu :\n1 - Challenger  2 - Défenseur  3 - Duel");
 
-            int reponseMode = sc.nextInt();
+            int answerMode = sc.nextInt();
 
-            if (reponseMode == 1) {
-                challengerBoucle    =  true;
+            if (answerMode == 1) {
+                challengerLoop    =  true;
             }
-            else if (reponseMode == 2) {
-                defenseurBoucle     = true;
+            else if (answerMode == 2) {
+                defenderLoop     = true;
             }
-            else if (reponseMode == 3) {
-                duelBoucle          = true;
+            else if (answerMode == 3) {
+                duelLoop          = true;
             }
 
-            while (challengerBoucle) {
-                ModesDeJeu.modeChallenger();
+            while (challengerLoop) {
+                GameModes.challengerMode();
             }
-            while (defenseurBoucle) {
-                ModesDeJeu.modeDefenseur();
+            while (defenderLoop) {
+                GameModes.defenderMode();
             }
-            while (duelBoucle) {
-                ModesDeJeu.modeDuel();
+            while (duelLoop) {
+                GameModes.duelMode();
             }
         }
     }
